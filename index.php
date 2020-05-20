47eebbabb124f51a9fdc250a6f814859db188300
mauros93 <62360326+mauros93@users.noreply.github.com>
2020-05-20 11:34:22 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mauricio Builes PHP</title>
+</head>
+<body>
+    <h1>Mauricio Builes PHP</h1>
+
+    <p>The SHA256 hash of "Mauricio Builes" is <?php print hash('sha256', 'Charles Severance');?></p>
+
+    <pre>ASCII ART:
+    ***********
+    **       **
+    **
+    **
+    **
+    **       **
+    ***********
+    </pre>
+    
+    <a href="check.php">Click here to check the error setting</a>
+    <br/>
+    <a href="fail.php">Click here to cause a traceback</a>
+    <br>
+    <?php $_GET['x'];
+    ?>
+    
+
+   
+</body>
+</html>
